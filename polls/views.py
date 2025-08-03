f3caf856fe0dd40ac79c9364bdc0f781105e7bbe
Crabby <makolramansh@gmail.com>
2025-08-03 15:00:58 +0530

    Made the question and the choices

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse(
+        "<p>Hey, everyone. I ask you to vote in this poll very politely.</p>"
+        "<p>This is a poll for my Instagram bio, as I can't decide which one I should put there.</p>"
+        "<p>To give you some context about me, I am a very extroverted and candid person, so go ham!</p>"
+    )
