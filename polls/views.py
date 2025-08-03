bdf0205a471b2742cede7b75f6645c057dd55ed5
Crabby <makolramansh@gmail.com>
2025-08-03 15:24:38 +0530

    Displayed the initial 4 choices

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# polls/views.py
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse(
-        "<p>Hey, everyone. I ask you to vote in this poll very politely.</p>"
-        "<p>This is a poll for my Instagram bio, as I can't decide which one I should put there.</p>"
-        "<p>To give you some context about me, I am a very extroverted and candid person, so go ham!</p>"
-    )
+    # Get your question with id=1 from the database
+    question = get_object_or_404(Question, pk=1)
+    # Send the question to the template
+    return render(request, 'polls/index.html', {'question': question})
+
+def results(request, question_id):
+    # Get the question and display the results page
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST['choice'] gets the ID of the choice selected by the user
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # If no choice was selected, show the poll form again with an error
+        return render(request, 'polls/index.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # Add one vote and save the change to the database
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Redirect to the results page after a successful vote.
+        # reverse() helps build the URL like '/polls/1/results/'
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
