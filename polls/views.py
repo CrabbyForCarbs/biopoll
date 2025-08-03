8c365bd7d02ccd03380bbf006efffd3e805f6d51
Crabby <makolramansh@gmail.com>
2025-08-03 17:40:30 +0530

    Final setup with the poll

--- polls/views.py
+++ polls/views.py
@@ -1,36 +1,46 @@
 # polls/views.py
 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.http import JsonResponse
+from django.db.models import F
+from django.db import transaction # Import the transaction module
 from .models import Question, Choice
 
 def index(request):
-    # Get your question with id=1 from the database
     question = get_object_or_404(Question, pk=1)
-    # Send the question to the template
     return render(request, 'polls/index.html', {'question': question})
 
-def results(request, question_id):
-    # Get the question and display the results page
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
+# This decorator ensures all database operations within the function are safe
+@transaction.atomic
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    
     try:
-        # request.POST['choice'] gets the ID of the choice selected by the user
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # If no choice was selected, show the poll form again with an error
-        return render(request, 'polls/index.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        # Add one vote and save the change to the database
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Redirect to the results page after a successful vote.
-        # reverse() helps build the URL like '/polls/1/results/'
-        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+        # Get the ID of the choice the user just clicked
+        selected_choice_id = int(request.POST.get('choice'))
+    except (KeyError, TypeError, ValueError):
+        return JsonResponse({'success': False, 'error': 'Invalid choice.'}, status=400)
+
+    session_key = f'voted_on_question_{question.id}'
+    previous_choice_id = request.session.get(session_key)
+
+    # Case 1: User is changing their vote from a previous one
+    if previous_choice_id is not None and previous_choice_id != selected_choice_id:
+        # Decrement the old choice and increment the new one
+        Choice.objects.filter(pk=previous_choice_id).update(votes=F('votes') - 1)
+        Choice.objects.filter(pk=selected_choice_id).update(votes=F('votes') + 1)
+    
+    # Case 2: User is voting for the first time
+    elif previous_choice_id is None:
+        Choice.objects.filter(pk=selected_choice_id).update(votes=F('votes') + 1)
+
+    # Case 3: User clicked the same choice again (previous_choice_id == selected_choice_id)
+    # We do nothing in this case.
+
+    # After any changes, update the user's session to remember the latest vote
+    request.session[session_key] = selected_choice_id
+    
+    # Return the latest vote counts for all choices on the question
+    all_choices = question.choice_set.all()
+    vote_counts = {choice.id: choice.votes for choice in all_choices}
+    return JsonResponse({'success': True, 'votes': vote_counts})
