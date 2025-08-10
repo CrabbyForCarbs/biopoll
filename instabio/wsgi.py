361b6f5f68496e69523dbff87b70bb1da63d56c8
Crabby <makolramansh@gmail.com>
2025-08-10 13:19:06 +0530

    Implement new Vercel deployment strategy

--- instabio/wsgi.py
+++ instabio/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for instabio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+# instabio/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instabio.settings')
 
+# Run build commands
+call_command('migrate')
+call_command('collectstatic', '--noinput')
+
+# This is the main application Vercel will use
 application = get_wsgi_application()
+
+# Vercel looks for an 'app' variable
+app = application
