ce533b627c735bbd03d3d63ee93d4456bf59d671
Crabby <makolramansh@gmail.com>
2025-08-14 14:21:30 +0530

    Downgrade to Python 3.11 for compatibility

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # build_files.sh
 
-# Use python3.12 to match the runtime in vercel.json
-python3.12 -m pip install -r requirements.txt
+# Use python3.11 to match the runtime in vercel.json
+python3.11 -m pip install -r requirements.txt
 
-python3.12 manage.py collectstatic --noinput
-python3.12 manage.py migrate
+python3.11 manage.py collectstatic --noinput
+python3.11 manage.py migrate
