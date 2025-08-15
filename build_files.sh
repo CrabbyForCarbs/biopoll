b859cec9efacecd4fe61d64fdf9521b1f56cafe2
Crabby <makolramansh@gmail.com>
2025-08-15 14:54:36 +0530

    Setuptools smthg ig

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
+
+# Install setuptools to provide the missing 'distutils' for Python 3.12+
+pip3 install setuptools
+
+# Now, install all other dependencies
 pip3 install -r requirements.txt
+
+# Collect static files
 python3 manage.py collectstatic --noinput
